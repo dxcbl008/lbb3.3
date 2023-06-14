e2a171dfcaad8471255b724d00947479d3a01e30
dxcbl008 <131538604+dxcbl008@users.noreply.github.com>
2023-06-14 15:06:13 +0800

    Add files via upload

---
+++ app/locales/index.ts
@@ -0,0 +1,66 @@
+import CN from "./cn";
+import { merge } from "../utils/merge";
+
+export type { LocaleType, RequiredLocaleType } from "./cn";
+
+export const AllLangs = ["cn",] as const;
+export type Lang = (typeof AllLangs)[number];
+
+export const ALL_LANG_OPTIONS: Record<Lang, string> = { cn: "简体中文", };
+
+const LANG_KEY = "lang";
+const DEFAULT_LANG = "cn";
+
+function getItem(key: string) {
+    try {
+        return localStorage.getItem(key);
+    } catch {
+        return null;
+    }
+}
+
+function setItem(key: string, value: string) {
+    try {
+        localStorage.setItem(key, value);
+    } catch { }
+}
+
+function getLanguage() {
+    try {
+        return navigator.language.toLowerCase();
+    } catch {
+        console.log("[Lang] failed to detect user lang.");
+        return DEFAULT_LANG;
+    }
+}
+
+export function getLang(): Lang {
+    const savedLang = getItem(LANG_KEY);
+
+    if (AllLangs.includes((savedLang ?? "") as Lang)) {
+        return savedLang as Lang;
+    }
+
+    const lang = getLanguage();
+
+    for (const option of AllLangs) {
+        if (lang.includes(option)) {
+            return option;
+        }
+    }
+
+    return DEFAULT_LANG;
+}
+
+export function changeLang(lang: Lang) {
+    setItem(LANG_KEY, lang);
+    location.reload();
+}
+
+const fallbackLang = CN;
+const targetLang = { cn: CN }[getLang()] as typeof CN;
+
+// if target lang missing some fields, it will use fallback lang string
+merge(fallbackLang, targetLang);
+
+export default fallbackLang as typeof CN;
